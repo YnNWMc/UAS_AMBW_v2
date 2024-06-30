c3ce643fac5df74aeb4c0f410faf1c100b19a2be
Yan Nathanael W <yanhato03@gmail.com>
2024-06-30 23:30:35 +0700

    x

---
+++ uas_ambw_c14210061/lib/models/pin.dart
@@ -0,0 +1,12 @@
+// pin.dart (or any appropriate name)
+import 'package:hive/hive.dart';
+
+part 'pin.g.dart';
+
+@HiveType(typeId: 1)
+class Pin {
+  @HiveField(0)
+  String pin;
+
+  Pin(this.pin);
+}
